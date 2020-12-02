cc63fad5fbbe503c93a1505c21a4a3f5821e4c30
Robert Roland <rob@metabase.com>
2020-12-01 16:34:39 -0800

    starting a simulated union inside the API

--- src/metabase/api/advanced_computation.clj
+++ src/metabase/api/advanced_computation.clj
@@ -20,9 +20,25 @@
     (throw (Exception. (str (tru "`database` is required for all queries.")))))
   (api/read-check Database database)
 
-  ;;TODO: make this use process-query-async, and run all four queries in parallel
-  (for [pivot-query (pivot/generate-queries query)]
-    (-> (qp/process-query (log/spy :error (assoc query :query (:query pivot-query))))
-        (assoc :breakout (:breakout pivot-query)))))
+  ;; aggregating the results (simulating a SQL union)
+  ;; 1. find the combination of all the columns (which is the union of :breakouts :fields :expressions)
+  ;; 2. run that largest query first.
+  ;; 3. Start returning from the API:
+  ;;    {:data {:native_form nil ;; doesn't work for the fact that we're generating multiple queries
+  ;;            :insights nil    ;; doesn't work for the fact that we're generating multiple queries
+  ;;            :cols <from first query> 
+  ;;            :results_metadata <from first query> 
+  ;;            :results_timezone <from first query>
+  ;;            :rows []         ;; map the :rows responses from *all* queries together, using lazy-cat, adding breakout indicator and additional columns / setting to nil as necessary
+  ;;            }
+  ;;     :row_count <a total count from the rows collection above
+  ;;     :status :completed}
+  
+  (let [all-queries     (pivot/generate-queries query)
+        reverse-compare (fn [a b] (compare b a))
+        sorted-queries  (log/spy :error (sort (comp < count :breakout) all-queries))]
+    (for [pivot-query (pivot/generate-queries query)]
+      (qp/process-query (assoc query :query (:query pivot-query))))
+    ))
 
 (api/define-routes)
--- test/metabase/api/advanced_computation_test.clj
+++ test/metabase/api/advanced_computation_test.clj
@@ -7,14 +7,15 @@
 (use-fixtures :once (fixtures/initialize :db))
 
 (deftest pivot-dataset-test
-  (mt/dataset sample-dataset
-    (testing "POST /api/advanced_computation/pivot/dataset"
-      (testing "Run a pivot table"
-        (let [result  ((mt/user->client :rasta) :post 200 "advanced_computation/pivot/dataset"
-                                                (mt/mbql-query orders
-                                                               {:aggregation [[:count] [:sum $orders.quantity]]
-                                                                :breakout    [[:fk-> $orders.user_id $people.state]
-                                                                              [:fk-> $orders.user_id $people.source]
-                                                                              [:fk-> $orders.product_id $products.category]]}))]
+  (mt/with-log-level :info
+   (mt/dataset sample-dataset
+     (testing "POST /api/advanced_computation/pivot/dataset"
+       (testing "Run a pivot table"
+         (let [result  ((mt/user->client :rasta) :post 200 "advanced_computation/pivot/dataset"
+                                                 (mt/mbql-query orders
+                                                                {:aggregation [[:count] [:sum $orders.quantity]]
+                                                                 :breakout    [[:fk-> $orders.user_id $people.state]
+                                                                               [:fk-> $orders.user_id $people.source]
+                                                                               [:fk-> $orders.product_id $products.category]]}))]
           ;; This resultset is going to be entirely too large to write a full equality assertion for.
-          (is (= 8 (count result))))))))
+           (is (= 8 (count result)))))))))
